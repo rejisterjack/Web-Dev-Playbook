90071b25772b9f3ae515ac05c1d88f2b35260a9f
Rupam Das <rejisterjack@gmail.com>
2023-12-19 01:05:01 +0530

    added async thunks

--- Web Dev Vishwas/redux-toolkit-cev/app/store.js
+++ Web Dev Vishwas/redux-toolkit-cev/app/store.js
@@ -1,6 +1,7 @@
 const { configureStore } = require("@reduxjs/toolkit")
 const cakeReducer = require("../features/cake/cakeSlice")
 const icecreamReducer = require("../features/icecream/icecreamSlice")
+const userReducer = require("../features/user/userSlice")
 const reduxLogger = require("redux-logger")
 
 const logger = reduxLogger.createLogger()
@@ -9,8 +10,9 @@ const store = configureStore({
   reducer: {
     cake: cakeReducer,
     icecream: icecreamReducer,
+    user: userReducer,
   },
-  middleware: (defaultMiddleware)=>[...defaultMiddleware(), logger]
+  middleware: (defaultMiddleware) => [...defaultMiddleware(), logger],
 })
 
 module.exports = store
--- Web Dev Vishwas/redux-toolkit-cev/index.js
+++ Web Dev Vishwas/redux-toolkit-cev/index.js
@@ -1,14 +1,18 @@
 const store = require("./app/store")
 const { cakeActions } = require("./features/cake/cakeSlice")
 const { icecreamActions } = require("./features/icecream/icecreamSlice")
+const { fetchUsers } = require("./features/user/userSlice")
 
 console.log("initial state", store.getState())
 const unsubscribe = store.subscribe(() => {
   // console.log("updated state", store.getState())
 })
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.restocked(4))
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.restocked(4))
 
-store.dispatch(icecreamActions.ordered())
-store.dispatch(icecreamActions.restocked(5))
-unsubscribe()
+// store.dispatch(icecreamActions.ordered())
+// store.dispatch(icecreamActions.restocked(5))
+
+store.dispatch(fetchUsers())
+
+// unsubscribe()
