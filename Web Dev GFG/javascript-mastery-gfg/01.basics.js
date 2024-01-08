6309b0b691422c275aa90a1de1600781938ef155
Rupam Das <rejisterjack@gmail.com>
2024-01-08 23:33:37 +0530

    added js basics

---
+++ Web Dev GFG/javascript-mastery-gfg/01.basics.js
@@ -0,0 +1,29 @@
+// data types
+// 1. primitive type => string, boolean, number, undefined, null, nan, bigint
+// 2. reference type => array, objects
+
+// arithmetic operators => +, -, *, /, %, **
+
+// const a = 12
+// const b = 3
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// console.log(a**b)
+
+// type conversion
+
+const a = 5
+console.log(typeof a)
+console.log(typeof String(a))
+// shortcut
+console.log(typeof (a + ""))
+
+const b= "10"
+console.log(typeof b)
+console.log(typeof Number(b))
+// shrotcut
+console.log(typeof +b)
