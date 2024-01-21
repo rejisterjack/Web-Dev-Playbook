9a9cb61a9ba1c7f1568b6a1abffdfac54450f17a
Rupam Das <rejisterjack@gmail.com>
2024-01-21 19:42:40 +0530

    starting with recoil

--- 100X Devs Cohort/07.01.routing/src/App.jsx
+++ 100X Devs Cohort/07.01.routing/src/App.jsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { lazy, Suspense } from "react"
+import { Route, Routes, useNavigate } from "react-router-dom"
+
+// Use lazy to import components lazily
+const Landing = lazy(() => import("./components/Landing"))
+const Dashboard = lazy(() => import("./components/Dashboard"))
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <AppBar />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Suspense>
+    </div>
+  )
+}
+
+function AppBar() {
+  const navigate = useNavigate()
+  return (
+    <div>
+      <button className="btn btn-primary m-2" onClick={() => navigate("/")}>
+        Landing
+      </button>
+      <button
+        className="btn btn-primary m-2"
+        onClick={() => navigate("/dashboard")}
+      >
+        Dashboard
+      </button>
+    </div>
   )
 }
 
---
+++ 100X Devs Cohort/07.02.recoil/src/store/atoms/count.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function count() {
+  return (
+    <div>count</div>
+  )
+}
+
+export default count
