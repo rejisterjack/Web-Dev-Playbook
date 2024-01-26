7f8c526ddda618457f88ea58d34718623053beed
Rupam Das <rejisterjack@gmail.com>
2024-01-27 01:07:59 +0530

    added prisma scripts

--- Web Dev Simplified/prisma-simplified/script.ts
+++ Web Dev Simplified/prisma-simplified/script.ts
@@ -3,6 +3,14 @@ const prisma = new PrismaClient()
 
 async function main() {
   await prisma.user.deleteMany()
+  const user = await prisma.user.create({
+    data: {
+      name: "rupam",
+      age: 23,
+      email: "rupam@gmail.com",
+    },
+  })
+  console.log(user)
 }
 
 main()
