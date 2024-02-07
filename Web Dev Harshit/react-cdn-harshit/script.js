527e478c758f6d027e3e9c99851daa8f12ee20fa
Rupam Das <rejisterjack@gmail.com>
2024-02-07 23:32:08 +0530

    reset

--- Web Dev Harshit/react-cdn-harshit/script.js
+++
@@ -1,7 +0,0 @@
-const rootElement = document.getElementById("root")
-
-const el = React.createElement("h2", { className: "heading" }, "heading two")
-const box = React.createElement("div", { className: "box" }, el)
-
-ReactDOM.createRoot(rootElement).render(el)
-ReactDOM.createRoot(rootElement).render(box)
