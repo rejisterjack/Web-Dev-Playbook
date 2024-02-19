5a9dff0863c0f845be208a3f15f1dc0ebd6b7865
Rupam Das <rejisterjack@gmail.com>
2024-02-20 01:07:50 +0530

    completed with express simplified

--- Web Dev Simplified/express-simplified/index.js
+++ Web Dev Simplified/express-simplified/index.js
@@ -2,11 +2,16 @@ const express = require("express")
 const app = express()
 
 app.set("view engine", "ejs")
+const userRouter = require("./routes/users")
 
-app.get("/", (req, res)=>{
-  res.render("index", {text: "World New"})
+app.use("/users", userRouter)
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+  res.render("index", { text: "World New" })
 })
 
-app.listen(8000, ()=>{
+app.listen(8000, () => {
   console.log("server is running...")
 })
+
