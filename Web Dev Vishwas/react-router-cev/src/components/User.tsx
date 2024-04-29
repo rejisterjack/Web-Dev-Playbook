8124b3b348694e533c634788378079c6e44c4923
Rupam Das <rejisterjack@gmail.com>
2024-04-29 12:11:25 +0530

    applied filters

--- Web Dev Vishwas/react-router-cev/src/components/User.tsx
+++ Web Dev Vishwas/react-router-cev/src/components/User.tsx
@@ -1,11 +1,13 @@
-import { MouseEventHandler } from "react"
-import { Outlet, useNavigate } from "react-router-dom"
+import { MouseEventHandler, useState } from "react"
+import { Outlet, useNavigate, useSearchParams } from "react-router-dom"
 
 const User = () => {
   const navigate = useNavigate()
   const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
     navigate(e.currentTarget.id)
   }
+  const [searchParams, setSearchParams] = useSearchParams()
+  const showActiveUsers = searchParams.get("filter") === "active"
   return (
     <div>
       <h2>Users</h2>
@@ -20,6 +22,24 @@ const User = () => {
           user 3
         </button>
       </div>
+      <h2 className="mt-2">Filters</h2>
+      <div className="d-flex gap-2">
+        <button
+          className="btn btn-primary"
+          id="1"
+          onClick={() => setSearchParams({ filter: "active" })}
+        >
+          active
+        </button>
+        <button
+          className="btn btn-primary"
+          id="2"
+          onClick={() => setSearchParams({})}
+        >
+          reset
+        </button>
+      </div>
+      {showActiveUsers && <p>Active Users</p>}
       <Outlet />
     </div>
   )
