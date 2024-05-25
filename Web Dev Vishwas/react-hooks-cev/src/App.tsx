ee11e51f4435094186bb70a1a7d57438a71382ae
Rupam Das <rejisterjack@gmail.com>
2024-05-25 21:06:18 +0530

    use effect with fetching

--- Web Dev Vishwas/react-hooks-cev/src/App.tsx
+++ Web Dev Vishwas/react-hooks-cev/src/App.tsx
@@ -3,15 +3,17 @@ import UseStateObject from "./components/02.UseStateObject"
 import UseStateArray from "./components/03.UseStateArray"
 import UseEffectHook from "./components/04.UseEffectHook"
 import UseEffectPointer, { TogglePointer } from "./components/05.UseEffectPointer"
+import UseEffectFetch from "./components/06.UseEffectFetch"
 
 const App = () => {
   return <div>
     {/* <UseStateHook /> */}
     {/* <UseStateObject /> */}
     {/* <UseStateArray /> */}
-    <UseEffectHook />
+    {/* <UseEffectHook /> */}
     {/* <UseEffectPointer /> */}
-    <TogglePointer />
+    {/* <TogglePointer /> */}
+    <UseEffectFetch />
   </div>
 }
 
