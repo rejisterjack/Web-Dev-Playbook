2149d1741bc2ee95e6efc72c7bbe00fd8517fa57
Rupam Das <rejisterjack@gmail.com>
2024-06-01 23:24:33 +0530

    radio buttons

--- Web Dev Harshit/react-form-harshit/src/App.jsx
+++ Web Dev Harshit/react-form-harshit/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 const App = () => {
   const [name, setName] = useState("")
   const [pass, setPass] = useState("")
+  const [gender, setGender] = useState("")
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -32,6 +33,46 @@ const App = () => {
             onChange={(e) => setPass(e.target.value)}
           />
         </div>
+        <div className="form-check">
+          <label className="label" htmlFor="male">
+            Male:
+          </label>
+          <input
+            type="radio"
+            className="form-check-input"
+            id="male"
+            // name="radio"
+            checked={gender==="Male"}
+            onChange={() => setGender("Male")}
+          />
+        </div>
+        <div className="form-check">
+          <label className="label" htmlFor="female">
+            Female:
+          </label>
+          <input
+            type="radio"
+            className="form-check-input"
+            id="female"
+            // name="radio"
+            checked={gender==="Female"}
+            onChange={() => setGender("Female")}
+          />
+        </div>
+        <div className="form-check">
+          <label className="label" htmlFor="others">
+            Others:
+          </label>
+          <input
+            type="radio"
+            className="form-check-input"
+            id="others"
+            // name="radio"
+            checked={gender==="Others"}
+            onChange={() => setGender("Others")}
+          />
+        </div>
+
         <input
           type="submit"
           value="Submit"
