30d8c1c9dda00724b7867070ba8627fcb74a947d
Rupam Das <rejisterjack@gmail.com>
2024-06-24 16:36:51 +0530

    dynamic form, number and date values

--- Web Dev Vishwas/react-hook-form-cev/src/components/02.EnhancedForm.tsx
+++ Web Dev Vishwas/react-hook-form-cev/src/components/02.EnhancedForm.tsx
@@ -1,27 +1,51 @@
-import { useForm } from "react-hook-form"
+import { useForm, useFieldArray } from "react-hook-form"
 import { DevTool } from "@hookform/devtools"
 
 type FormValues = {
   username: string
   email: string
   channel: string
+  social: {
+    facebook: string
+    twitter: string
+  }
+  phoneNumbers: string[]
+  phNumbers?: {
+    number: string
+    key: number
+  }[]
+  age: number
+  dob: Date
 }
 
 const EnhancedForm = () => {
   const { register, handleSubmit, control, formState } = useForm<FormValues>({
     // normal values
-    // defaultValues: {
-    //   username: "John Doe",
-    //   email: "",
-    //   channel: "",
-    // },
+    defaultValues: {
+      username: "John Doe",
+      email: "",
+      channel: "",
+      social: {
+        facebook: "",
+        twitter: "",
+      },
+      phoneNumbers: ["", ""],
+      phNumbers: [{ number: "", key: 0 }],
+      age: 0,
+      dob: new Date(),
+    },
 
     // async values
-    defaultValues: async () => {
-      const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
-      const data = await response.json()
-      return data
-    }
+    // defaultValues: async () => {
+    //   const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+    //   const data = await response.json()
+    //   return data
+    // }
+  })
+
+  const { fields, append, remove } = useFieldArray({
+    name: "phNumbers",
+    control,
   })
 
   const onSubmit = (data: FormValues) => {
@@ -83,6 +107,104 @@ const EnhancedForm = () => {
             })}
           />
           <span>{formState.errors.channel?.message}</span>
+        </div>
+        <div className="form-group">
+          <label htmlFor="facebook">Facebook</label>
+          <input
+            type="text"
+            className="form-control"
+            id="facebook"
+            {...register("social.facebook")}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="twitter">Twitter</label>
+          <input
+            type="text"
+            className="form-control"
+            id="twitter"
+            {...register("social.twitter")}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="phoneNumbers">Phone Number 2</label>
+          <input
+            type="text"
+            className="form-control"
+            id="phoneNumbers"
+            {...register("phoneNumbers.0")}
+          />
+          <label htmlFor="phoneNumbers">Phone Number 1</label>
+          <input
+            type="text"
+            className="form-control"
+            id="phoneNumbers"
+            {...register("phoneNumbers.1")}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="phNumbers">List of Phone Numbers</label>
+          {fields.map((field, index) => (
+            <div key={field.id}>
+              <input
+                type="text"
+                className="form-control"
+                {...register(`phNumbers.${index}.number` as const)}
+              />
+              {index > 0 && (
+                <button
+                  type="button"
+                  className="btn btn-danger"
+                  onClick={() => remove(index)}
+                >
+                  Remove
+                </button>
+              )}
+            </div>
+          ))}
+          <button
+            type="button"
+            className="btn btn-primary btn-sm mt-2"
+            onClick={() => {
+              append({ number: "", key: fields.length })
+            }}
+          >
+            Add Phone Number
+          </button>
+        </div>
+        <div className="form-group">
+          <label htmlFor="age">Age</label>
+          <input
+            type="number"
+            className="form-control"
+            id="age"
+            {...register("age", {
+              valueAsNumber: true,
+              required: "Age is required",
+              min: {
+                value: 18,
+                message: "Minimum age is 18",
+              },
+            })}
+          />
+          <span>{formState.errors.age?.message}</span>
+        </div>
+        <div className="form-group">
+          <label htmlFor="dob">Date of Birth</label>
+          <input
+            type="date"
+            className="form-control"
+            id="dob"
+            {...register("dob", {
+              required: "Date of Birth is required",
+              valueAsDate: true,
+              validate: {
+                pastDate: (value) =>
+                  new Date(value) < new Date() || "Invalid date",
+              },
+            })}
+          />
+          <span>{formState.errors.dob?.message}</span>
         </div>
         <button type="submit" className="btn btn-primary mt-2">
           Submit
