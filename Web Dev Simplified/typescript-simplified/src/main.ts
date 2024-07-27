88a738a3f1c6f17f1a2dbcff84ff9843bc5e4cb6
Rupam Das <rejisterjack@gmail.com>
2024-07-27 21:13:05 +0530

    typescript object and functions

--- Web Dev Simplified/typescript-simplified/src/main.ts
+++ Web Dev Simplified/typescript-simplified/src/main.ts
@@ -1,24 +1,44 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+// objects
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+type Person = {
+  name: string
+  age: number
+  isDeveloper?: boolean
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+let obj: Person = {
+  name: "John",
+  age: 20,
+}
+
+obj.isDeveloper = true
+
+console.log(obj)
+
+// function
+
+type Add = (a: number, b: number) => number
+type Sub = (a: number, b: number) => void // function that does not return anything
+
+const add: Add = (a, b) => a + b
+const sub: Sub = (a, b) => console.log(a - b)
+
+// functions with default values
+const multiply = (a: number, b: number = 1) => a * b
+const divide = (a: number, b: number = 1) => a / b
+
+// functions with optional values
+const sayHello = (name?: string) => {
+  if (name) {
+    console.log(`Hello ${name}`)
+  } else {
+    console.log("Hello")
+  }
+}
+
+// functions with rest parameters
+const sum = (...numbers: number[]) => {
+  return numbers.reduce((acc, val) => acc + val, 0)
+}
+
+// 16. union types to be started
