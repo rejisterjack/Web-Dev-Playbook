8aa36efe17eb077d26c442dcaf182ce6a44fd90f
Rupam Das <rejisterjack@gmail.com>
2024-07-28 15:16:41 +0530

    basic typescript types

--- Web Dev Simplified/typescript-simplified/src/main.ts
+++ Web Dev Simplified/typescript-simplified/src/main.ts
@@ -41,4 +41,114 @@ const sum = (...numbers: number[]) => {
   return numbers.reduce((acc, val) => acc + val, 0)
 }
 
-// 16. union types to be started
+// union types
+type Status = "success" | "error" | "pending"
+type TestUnion = string | { text: string }
+
+// type and interface
+interface User {
+  name: string
+  age: number
+}
+
+type User2 = {
+  name: string
+  age: number
+  child: User
+}
+
+// extending interfaces
+interface Admin extends User {
+  role: string
+}
+
+// extending types
+type Admin2 = User & {
+  role: string
+}
+
+// intersection types
+type Admin3 = User & {
+  role: string
+}
+
+// type guards
+const isAdmin = (user: User | Admin): user is Admin => {
+  return (user as Admin).role !== undefined
+}
+
+// discriminated unions
+type Success = {
+  status: "success"
+  data: string
+}
+
+type Error = {
+  status: "error"
+  message: string
+}
+
+type Response = Success | Error
+
+// type aliases
+type ID = string | number
+
+// string literals
+type Direction = "left" | "right" | "up" | "down"
+
+// numeric literals
+type OneToFive = 1 | 2 | 3 | 4 | 5
+
+// boolean literals
+type True = true
+type False = false
+
+// function overloads
+function reverse(x: number): number
+function reverse(x: string): string
+function reverse(x: number | string): number | string {
+  if (typeof x === "number") {
+    return Number(x.toString().split("").reverse().join(""))
+  } else if (typeof x === "string") {
+    return x.split("").reverse().join("")
+  }
+}
+
+// type assertions
+const user = {} as User
+const user2 = <User>{}
+
+// keyof and typeof
+type UserKeys = keyof User
+type UserTypes = typeof User
+
+// index types 
+type UserProps = keyof User
+type UserTypes2 = User[UserProps]
+
+// as const and readonly and enum
+const colors = {
+  red: "red",
+  blue: "blue",
+  green: "green",
+} as const
+
+// typle
+type Tuple = [number, string, boolean]
+
+// generics
+// type ValueOf<T> = T[keyof T]
+// const input = document.querySelector("input") as HTMLInputElement
+const input = document.querySelector<HTMLInputElement>("input")
+console.log(input?.value)
+
+function getSecond<T>(array: T[]) {
+  return array[1]
+}
+const a = [1, 2, 3]
+const b = ["a", "b", "c"]
+
+const retA = getSecond(a)
+const retB = getSecond(b)
+
+// generic to be completed
