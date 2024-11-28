34c6e58e3a52284f3166462fb3204dba7d439b1e
Rupam Das <rejisterjack@gmail.com>
2024-11-28 19:49:37 +0530

    setup

--- Web Dev Simplified/testing-simplified/vite.config.ts
+++ Web Dev Simplified/testing-simplified/vite.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
-    globals: true,
     setupFiles: "setupTests.js",
     includes: ["src/**/*.test.tsx"],
   },
