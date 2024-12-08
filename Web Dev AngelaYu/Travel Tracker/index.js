50223a2a9874bab83077eafe394b8c7597071cdb
Rupam Das <rejisterjack@gmail.com>
2024-12-08 10:29:58 +0530

    travel tracker

--- Web Dev AngelaYu/Travel Tracker/index.js
+++ Web Dev AngelaYu/Travel Tracker/index.js
@@ -3,12 +3,12 @@ import bodyParser from "body-parser"
 import pg from "pg"
 
 const app = express()
-const port = 8000
+const port = 3000
 
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "postgres",
+  database: "world",
   password: "8082",
   port: 5432,
 })
@@ -19,7 +19,29 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
 
 app.get("/", async (req, res) => {
-  //Write your code here.
+  const result = await db.query("SELECT country_code from visited_countries")
+  let countries = result.rows.map((row) => row.country_code)
+  res.render("index.ejs", { countries, total: countries.length })
+  db.end()
+})
+
+app.post("/add", async (req, res) => {
+  const input = req.body["country"]
+
+  const result = await db.query(
+    "SELECT country_code FROM countries WHERE country_name = $1",
+    [input]
+  )
+
+  if (result.rows.length !== 0) {
+    const data = result.rows[0]
+    const countryCode = data.country_code
+
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+      countryCode,
+    ])
+    res.redirect("/")
+  }
 })
 
 app.listen(port, () => {
