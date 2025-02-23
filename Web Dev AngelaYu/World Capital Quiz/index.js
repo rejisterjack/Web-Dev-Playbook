0e63b27268857df98aab31068516a7453d44038f
Rupam Das <rejisterjack@gmail.com>
2025-02-23 22:54:28 +0530

    cleanup

--- Web Dev AngelaYu/World Capital Quiz/index.js
+++ Web Dev AngelaYu/World Capital Quiz/index.js
@@ -1,55 +1,74 @@
-import express from "express";
-import bodyParser from "body-parser";
+import express from "express"
+import bodyParser from "body-parser"
+import pg from "pg"
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 8000
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "postgres",
+  password: "8082",
+  port: 5432,
+})
 
-let quiz = [
-  { country: "France", capital: "Paris" },
-  { country: "United Kingdom", capital: "London" },
-  { country: "United States of America", capital: "New York" },
-];
+let quiz = []
 
-let totalCorrect = 0;
+db.query("SELECT * FROM capitals", (err, res) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  quiz = res.rows
+})
+
+let totalCorrect = 0
 
 // Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static("public"))
 
-let currentQuestion = {};
+let currentQuestion = {}
 
 // GET home page
 app.get("/", async (req, res) => {
-  totalCorrect = 0;
-  await nextQuestion();
-  console.log(currentQuestion);
-  res.render("index.ejs", { question: currentQuestion });
-});
+  totalCorrect = 0
+  await nextQuestion()
+  console.log(currentQuestion)
+  res.render("index.ejs", { question: currentQuestion })
+})
 
 // POST a new post
 app.post("/submit", (req, res) => {
-  let answer = req.body.answer.trim();
-  let isCorrect = false;
+  let answer = req.body.answer.trim() || "No answer provided"
+  let isCorrect = false
   if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
-    totalCorrect++;
-    console.log(totalCorrect);
-    isCorrect = true;
+    totalCorrect++
+    console.log(totalCorrect)
+    isCorrect = true
   }
 
-  nextQuestion();
+  nextQuestion()
   res.render("index.ejs", {
     question: currentQuestion,
     wasCorrect: isCorrect,
     totalScore: totalCorrect,
-  });
-});
+  })
+})
 
 async function nextQuestion() {
-  const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
+  const randomCountry = quiz[Math.floor(Math.random() * quiz.length)]
 
-  currentQuestion = randomCountry;
+  currentQuestion = randomCountry
 }
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+db.connect((err) => {
+  if (err) {
+    console.error("connection error", err.stack)
+  } else {
+    console.log("Database connected!")
+    app.listen(port, () => {
+      console.log(`Server is running at http://localhost:${port}`)
+    })
+  }
+})
--- Web Dev AngelaYu/World Capital Quiz/solution.js
+++ Web Dev AngelaYu/World Capital Quiz/solution.js
@@ -1,69 +1,69 @@
-import express from "express";
-import bodyParser from "body-parser";
-import pg from "pg";
+import express from "express"
+import bodyParser from "body-parser"
+import pg from "pg"
 
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "world",
-  password: "123456",
+  database: "postgres",
+  password: "8082",
   port: 5432,
-});
+})
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 8000
 
-db.connect();
+db.connect()
 
-let quiz = [];
+let quiz = []
 db.query("SELECT * FROM capitals", (err, res) => {
   if (err) {
-    console.error("Error executing query", err.stack);
+    console.error("Error executing query", err.stack)
   } else {
-    quiz = res.rows;
+    quiz = res.rows
   }
-  db.end();
-});
+  db.end()
+})
 
-let totalCorrect = 0;
+let totalCorrect = 0
 
 // Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static("public"))
 
-let currentQuestion = {};
+let currentQuestion = {}
 
 // GET home page
 app.get("/", async (req, res) => {
-  totalCorrect = 0;
-  await nextQuestion();
-  console.log(currentQuestion);
-  res.render("index.ejs", { question: currentQuestion });
-});
+  totalCorrect = 0
+  await nextQuestion()
+  console.log(currentQuestion)
+  res.render("index.ejs", { question: currentQuestion })
+})
 
 // POST a new post
 app.post("/submit", (req, res) => {
-  let answer = req.body.answer.trim();
-  let isCorrect = false;
+  let answer = req.body.answer.trim()
+  let isCorrect = false
   if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
-    totalCorrect++;
-    console.log(totalCorrect);
-    isCorrect = true;
+    totalCorrect++
+    console.log(totalCorrect)
+    isCorrect = true
   }
 
-  nextQuestion();
+  nextQuestion()
   res.render("index.ejs", {
     question: currentQuestion,
     wasCorrect: isCorrect,
     totalScore: totalCorrect,
-  });
-});
+  })
+})
 
 async function nextQuestion() {
-  const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
-  currentQuestion = randomCountry;
+  const randomCountry = quiz[Math.floor(Math.random() * quiz.length)]
+  currentQuestion = randomCountry
 }
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+  console.log(`Server is running at http://localhost:${port}`)
+})
