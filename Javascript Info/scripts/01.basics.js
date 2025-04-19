2e1046da900d98f8076d90251e4679fc9d131591
Rupam Das <rejisterjack@gmail.com>
2025-04-20 00:51:37 +0530

    rust init

--- Javascript Info/scripts/01.basics.js
+++ Javascript Info/scripts/01.basics.js
@@ -1,7 +1,11 @@
+"use strict"
+
 // alert(3 +
 //     1
 //     + 2);
 
-alert("There will be an error");
-[1, 2].forEach(alert)
+// alert("There will be an error");
+// [1, 2].forEach(alert)
 
+console.log(Number(null))
+console.log(Number(undefined))
