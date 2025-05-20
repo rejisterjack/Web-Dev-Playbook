bef82d6b14b74007618121f0ab0555b5ef143a8d
Rupam Das <rejisterjack@gmail.com>
2025-05-20 09:03:45 +0530

    header

--- 00 Practice Playbooks/react-auth/client/src/pages/Home.jsx
+++ 00 Practice Playbooks/react-auth/client/src/pages/Home.jsx
@@ -5,26 +5,30 @@ const Home = () => {
   const { logout, userData } = useAuth()
   console.log(userData)
   return (
-    <div>
-      <p>Home</p>
-      <p>Welcome to the home page!</p>
-      <button onClick={logout}>Logout</button>
-      <header className='w-full flex items-center justify-center'>
-        <div className='w-full md:w-3/4 p-3 bg-gray-100 rounded-lg flex items-center justify-between'>
-          <p>rejisterjack</p>
-          <div className='flex items-center justify-center'>
-            <div className='w-12 h-12 rounded-full relative cursor-pointer'>
-              <img
-                src={userData?.photoURL}
-                alt=''
-                referrerPolicy='no-referrer'
-                className='w-full h-full object-cover rounded-full'
-              />
-            </div>
+    <header className='w-full flex items-center justify-center'>
+      <div className='w-full md:w-3/4 p-3 bg-gray-100 rounded-lg flex items-center justify-between'>
+        <div className='flex items-center gap-2'>
+          <p>{userData?.displayName}</p>
+          <p>{userData?.email}</p>
+          <button
+            onClick={logout}
+            className='bg-gray-100 shadow-md rounded-md py-2 px-4 cursor-pointer'
+          >
+            Logout
+          </button>
+        </div>
+        <div className='flex items-center justify-center'>
+          <div className='w-12 h-12 rounded-full relative cursor-pointer'>
+            <img
+              src={userData?.photoURL}
+              alt=''
+              referrerPolicy='no-referrer'
+              className='w-full h-full object-cover rounded-full'
+            />
           </div>
         </div>
-      </header>
-    </div>
+      </div>
+    </header>
   )
 }
 
