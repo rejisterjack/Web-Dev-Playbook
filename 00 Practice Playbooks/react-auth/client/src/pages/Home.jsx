4a873b907915f2b7802e454d7e3018e242abe877
Rupam Das <rejisterjack@gmail.com>
2025-05-20 08:37:44 +0530

    logout

--- 00 Practice Playbooks/react-auth/client/src/pages/Home.jsx
+++ 00 Practice Playbooks/react-auth/client/src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useAuth } from '../context/AuthContext'
 
 const Home = () => {
+  const { logout } = useAuth()
   return (
-    <div>Home</div>
+    <div>
+      <p>Home</p>
+      <p>Welcome to the home page!</p>
+      <button onClick={logout}>Logout</button>
+    </div>
   )
 }
 
