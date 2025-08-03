11dae76d255bbf8d8152261f300fead256e76289
Rupam Das <rejisterjack@gmail.com>
2025-08-03 20:12:38 +0530

    progress bar

---
+++ 00 Practice Playbooks/complete-nodejs/index.js
@@ -0,0 +1,27 @@
+// console.log('Hello there, my name is %s and i am %d years old', 'rupam', 24)
+
+// const sum = () => console.log(`sum of 2 + 3 is ${2 + 3}`)
+// const multiply = () => console.log(`multiply of 2 * 3 is ${2 * 3}`)
+
+// const measureTime = () => {
+//   console.time('sum()')
+//   sum()
+//   console.timeEnd('sum()')
+//   console.time('multiply()')
+//   multiply()
+//   console.timeEnd('multiply()')
+// }
+
+// measureTime()
+
+// const ProgressBar = require('progress')
+// const bar = new ProgressBar('downloading [:bar] :rate/bps :percent :etas', {
+//   total: 20,
+// })
+
+// const progInterval = setInterval(() => {
+//   bar.tick()
+//   if (bar.complete) clearInterval(progInterval)
+// }, 100)
+
+
