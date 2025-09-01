531e019f52744631885d3699111b037f8b7f1f07
Rupam Das <rejisterjack@gmail.com>
2025-09-02 01:16:02 +0530

    installation

--- 00 Practice Playbooks/react-hooks-simplified/src/hooks/03.UseEffectHook.tsx
+++ 00 Practice Playbooks/react-hooks-simplified/src/hooks/03.UseEffectHook.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 const UseEffectHook = () => {
-	return <div>UseEffectHook</div>;
+	const [width, setWidth] = useState(window.innerWidth);
+
+	useEffect(() => {
+		const handleWidth = () => {
+			setWidth(window.innerWidth);
+		};
+
+		window.addEventListener("resize", handleWidth);
+		return () => {
+			window.removeEventListener("resize", handleWidth);
+		};
+	}, []);
+
+	return (
+		<div>
+			<h1 className="p-4">{width}</h1>
+		</div>
+	);
 };
 
 export default UseEffectHook;
