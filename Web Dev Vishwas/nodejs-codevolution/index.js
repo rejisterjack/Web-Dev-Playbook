662f58c1a5deaaf423438c21102e0121406952e7
Rupam Das <rejisterjack@gmail.com>
2025-09-19 23:48:57 +0530

    node: local variables

--- Web Dev Vishwas/nodejs-codevolution/index.js
+++ Web Dev Vishwas/nodejs-codevolution/index.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+// ;(function (hero) {
+//   console.log(hero)
+// })("my batman")
+// ;(function () {
+//   console.log('superman')
+// })()
+
+const SuperHero = require("./superhero")
+
+const myHero = new SuperHero()
+console.log(myHero.getHero())
+myHero.setHero("batman")
+console.log(myHero.getHero())
---
+++ Web Dev Vishwas/nodejs-codevolution/superhero.js
@@ -0,0 +1,15 @@
+class SuperHero {
+  constructor() {
+    this.hero = 'superman'
+  }
+
+  getHero() {
+    return this.hero
+  }
+
+  setHero(hero) {
+    this.hero = hero
+  }
+}
+
+module.exports = SuperHero
