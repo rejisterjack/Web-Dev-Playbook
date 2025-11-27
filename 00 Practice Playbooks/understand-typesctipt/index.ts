c0a6bfa2686d9da6bcabd92dcf2f67d46c6f49d2
Rupam Das <rejisterjack@gmail.com>
2025-11-28 00:40:54 +0530

    cleanup

---
+++ 00 Practice Playbooks/understand-typesctipt/index.ts
@@ -0,0 +1,17 @@
+enum Role {
+  Admin,
+  User,
+  Guest
+}
+
+const userRole:Role = Role.Admin
+
+const user: Record<string, string | number> = {
+  name: "rupam",
+  age: 25
+}
+
+const fruits: Array<string> = [
+
+]
+
