1f566968545c3765f82dfb676a0e4fcdd50080df
Rupam Das <rejisterjack@gmail.com>
2025-12-27 15:07:59 +0530

    manual not founc

--- Web Dev Vishwas/nextjs-codevolution/app/products/[productId]/page.tsx
+++ Web Dev Vishwas/nextjs-codevolution/app/products/[productId]/page.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { notFound } from 'next/navigation'
 
 const page = async ({ params }: { params: Promise<{ productId: string }> }) => {
   const productId = (await params).productId
+  if(productId === '1000') notFound()
   return <div>products details page {productId}</div>
 }
 
